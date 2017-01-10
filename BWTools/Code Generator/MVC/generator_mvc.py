013038726083e4694de3ac4b13aa1c56c5811c01
wangbo <wangbo@bluemoon.com.cn>
2017-01-10 12:10:39 +0800

    Modify

--- BWTools/Code Generator/MVC/generator_mvc.py
+++ BWTools/Code Generator/MVC/generator_mvc.py
@@ -1,6 +1,7 @@
 
 # coding: utf-8
 # author: BobWong
+# Use for: Generate MVC file efficiently
 
 ' MVC Generator '
 
@@ -16,6 +17,9 @@ import shutil
 
 project_name = 'BWiOSProject'
 copyright_name = 'BobWongStudio'
+mvc_module_name = 'MVC'
+#mvc_module_name = sys.argv[1]
+prefix_name = 'BW'
 author_name = 'BobWong'
 import_file = '#import <UIKit/UIKit.h>'
 base_vc = 'UIViewController'
@@ -26,10 +30,14 @@ path_source_file = 'Source/source.txt'
 path_generation = 'Generation'
 path_generation_history = '%s/History' % path_generation
 path_temp = '%s/Temporary' % path_generation
-path_mvc = '%s/MVC' % path_temp
+path_mvc = '%s/%s' % (path_temp, mvc_module_name)
+
+name_controller = 'Controller'
+name_model = 'Model'
+name_view = 'View'
 
 year_string = time.strftime('%Y')  # 获得当前年份
-date_string = time.strftime("%Y/%m/%d")  # 获得当前日期，转换为字符串
+date_string = time.strftime("%y/%m/%d")  # 获得当前日期，转换为字符串
 
 # Function Definition
 
@@ -57,16 +65,20 @@ def getTuple(line):
 # 创建VC
 def generateMVC(Source):
     (vc, title, comment) = getTuple(Source)
-#    print 'vc: %s, title: %s, comment: %s' % (vc, title, comment)
-    vc_name = 'BW%sController' % vc
+    vc_name = '%s%sController' % (prefix_name, vc)
     
+    # 创建vc文件目录
     path_vc = '%s/%s' % (path_mvc, vc_name)
-    
-#    os.mkdir(path_mvc)
+    path_controller = '%s/%s' % (path_vc, name_controller)
+    path_model = '%s/%s' % (path_vc, name_model)
+    path_view = '%s/%s' % (path_vc, name_view)
     os.mkdir(path_vc)
+    os.mkdir(path_controller)
+    os.mkdir(path_model)
+    os.mkdir(path_view)
     
     # vc头文件.h
-    file_vc_header = open('%s/%s.h' % (path_vc, vc_name), 'wb', 1)
+    file_vc_header = open('%s/%s.h' % (path_controller, vc_name), 'wb', 1)
     content_header = (
                       '//\n' +
                       '//  %s.h\n' % (vc_name) +
@@ -86,7 +98,7 @@ def generateMVC(Source):
     file_vc_header.close()
 
     # vc实现文件.m
-    file_vc_implement = open('%s/%s.m' % (path_vc, vc_name), 'wb', 1)
+    file_vc_implement = open('%s/%s.m' % (path_controller, vc_name), 'wb', 1)
     content_implement = (
                          '//\n' +
                          '//  %s.m\n' % (vc_name) +
@@ -102,12 +114,74 @@ def generateMVC(Source):
                          '\n' +
                          '<#Code#>\n' +
                          '\n' +
+                         '@end\n' +
+                         '\n' +
+                         '@implementation %s\n' % vc_name +
+                         '\n' +
+                         '#pragma mark - View Cycle\n' +
+                         '\n' +
+                         '- (void)viewDidLoad\n' +
+                         '{\n' +
+                         '    [super viewDidLoad];\n' +
+                         '    \n' +
+                         '    self.title = @"%s";\n' % title +
+                         '    \n' +
+                         '    [self setData];\n' +
+                         '    [self setUI];\n' +
+                         '    [self setConstraints];\n' +
+                         '    [self loadData];\n' +
+                         '}\n' +
+                         '\n' +
+                         '#pragma mark - Public Method\n' +
+                         '\n' +
+                         '#pragma mark - Action\n' +
+                         '\n' +
+                         '#pragma mark - Network\n' +
+                         '\n' +
+                         '#pragma mark - Custom Delegate\n' +
+                         '\n' +
+                         '#pragma mark - System Delegate\n' +
+                         '\n' +
+                         '#pragma mark - Private Method\n' +
+                         '\n' +
+                         '- (void)setData\n' +
+                         '{\n' +
+                         '    <#Code#>\n' +
+                         '}\n' +
+                         '\n' +
+                         '- (void)setUI\n' +
+                         '{\n' +
+                         '    <#Code#>\n' +
+                         '}\n' +
+                         '\n' +
+                         '- (void)setConstraints\n' +
+                         '{\n' +
+                         '    <#Code#>\n' +
+                         '}\n' +
+                         '\n' +
+                         '- (void)loadData\n' +
+                         '{\n' +
+                         '    <#Code#>\n' +
+                         '}\n' +
+                         '\n' +
+                         '#pragma mark - Getter and Setter\n' +
+                         '\n' +
                          '@end\n'
                          )
+
     file_vc_implement.write(content_implement)
     file_vc_implement.close()
 
+    # 占坑文件
+    file_model = open('%s/%s' % (path_model, name_model), 'wb', 1)
+    file_model.close()
+
+    file_view = open('%s/%s' % (path_view, name_view), 'wb', 1)
+    file_view.close()
+
+
 # Tool
+# 去除空格
 def stripSpace(string):
     return string.replace(' ', '')
 
@@ -120,10 +194,22 @@ def main():
     array_line = file.readlines()
     file.close()
     
+    # 有旧目录
+    if os.path.exists(path_mvc):
+        print 'Directory ' + path_mvc + ' exits!'
+        print 'Move Old ' + path_mvc + ' to History!\n'
+        if os.path.exists(path_generation_history + '/' + mvc_module_name):
+            shutil.rmtree(path_mvc)  # 移除
+        else:
+            shutil.move(path_mvc,path_generation_history)  # 移动
+
+    # 创建目录
+    os.mkdir(path_mvc)
+    
     for line in array_line:
         generateMVC(line)
 
-    print '完成MVC的生成操作'
+    print 'MVC Generation Finished'
 
 
 if __name__=='__main__':
